4bc29f1668a3f4aa0c7dde8c3ac73875a257ab43
Anjana Roy <157599116+anjana241@users.noreply.github.com>
2026-06-29 13:09:26 +0530

    Added Datatypes.java

---
+++ datatypes/Datatypes.java
@@ -0,0 +1,22 @@
+public class Employee{
+
+    public static void main(String[] args){
+
+        	String name = "Garret Graham";
+        	int age = 22;
+        	double height = 5.7;
+        	float weight = 70f;
+        	String salary = "$200";
+        	boolean time = true;
+        	
+System.out.println("Name : "+name);
+System.out.println("Age : "+age);
+System.out.println("Height : "+height +"feet");
+System.out.println("Weight : "+weight +"kg");
+System.out.println("Salary : "+salary);
+System.out.println("Part-time : "+time);	
+
+
+
+  }
+}
